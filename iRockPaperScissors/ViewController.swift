ebdb3e0530f9479d60b2d5462f41921819fd1e47
Marty Avedon <martyavedon@C4Qs-MacBook-Pro-72.local>
2016-09-29 21:19:58 -0400

    Initial Commit

---
+++ iRockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iRockPaperScissors
+//
+//  Created by Marty Avedon on 9/29/16.
+//  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
